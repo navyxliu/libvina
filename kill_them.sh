0741caa06b001ad5baf2b1b0d08b3f70e8514309
xliu <xliu@harpertown.sjtu.edu.cn>
2009-10-08 15:23:00 +0800

    fixed a bug in cleanup function; add ipcrm in cleanup.sh script

--- kill_them.sh
+++ kill_them.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 #when debuging the library, my programs might accidently generate a lot of zombies.
-#this script kill them all.
-#navy.xliu@gmail.com
+#this script kill them all.navy.xliu@gmail.com
 #2009/09/20
 for p in `ps aux | grep $1 | awk -F ' ' '{print $2}' `
 do
